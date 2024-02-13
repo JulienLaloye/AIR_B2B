7c85c99f3a9fe91e3e32a7ff7e142ad0e9f788a6
Sarida Pinyoying <jrysrd@gmail.com>
2024-02-13 19:16:58 +0100

    homepage

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="swiper"
+export default class extends Controller {
+  connect() {
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'vertical',
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }
+}
